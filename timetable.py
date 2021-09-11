2a46a4df5aa9e10fad0fd791b1d09db58e1c98bb
AveZorgen <KulikovArt2015@gmail.com>
2021-09-11 21:56:20 +0300

    3.1
    new requests (groups + shedule)

--- timetable.py
+++ timetable.py
@@ -2,25 +2,28 @@ import datetime
 import requests
 import time
 
-groups = ['34883', '34884', '34885', '36374', '36436']
+groups = ['3821Б1ФИ1', '3821Б1ФИ2', '3821Б1ФИ3', '3821Б1ФИ4', '3821Б1ФИ5']
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 YaBrowser/21.3.3.230 Yowser/2.5 Safari/537.36'}
 
 
-def timetable(group, date):
-    st, fin = st_fin(date)
-    url = 'https://portal.unn.ru/ruzapi/schedule/group/' + groups[group]
-    response = requests.get(url, headers=HEADERS, params={
-        'start': st,  # 'start': '2021.09.06',
-        'finish': fin,  # 'finish': '2021.09.12',
+def timetable(group,date):
+    url = 'https://portal.unn.ru/ruzapi/schedule/group/' + group_id(group)
+    response = requests.get(url,headers=HEADERS,  params={
+        'start': date.replace('-', '.'),  # 'start': '2021.09.06',
+        'finish': date.replace('-', '.'),  # 'finish': '2021.09.12',
         'lng': 1
     }).json()
-    table_need = [
-        {j: i[j] for j in i.keys()}
-        for i in response if i['date'] == date.replace('-', '.')]
+    return response
 
-    return table_need
-
+def group_id(group):
+    url = 'https://portal.unn.ru/ruzapi/search'
+    response = requests.get(url, headers=HEADERS, params={
+        'type': 'group',
+        'term': group.encode()
+    })
+    id = response.json()[0]['id']
+    return id
 
 def print_table(table, date):
     print('Расписание на', date)
@@ -58,19 +61,6 @@ def table_chat(table, date):
     return chat_array
 
 
-def st_fin(date):
-    date_s = list(map(int, date.split('-')))
-    today = datetime.date(*date_s)
-    for i in range(7):
-        a = today + datetime.timedelta(days=-i)
-        if datetime.datetime.isoweekday(a) == 1:
-            break
-    b = a + datetime.timedelta(days=6)
-    st = str(a).replace('-', '.')
-    fin = str(b).replace('-', '.')
-    return [st, fin]
-
-
 def time_posting(std_time):
     hour, minutes = map(int, std_time.split(':'))
 
@@ -102,7 +92,7 @@ def time_client(client, channels):
         while True:
             DATE, TIME = str(datetime.datetime.now() + datetime.timedelta(days=1)).split()
             for i, j in enumerate(channels):
-                GROUP = i
+                GROUP = groups[i]
                 CHANNEL = j
                 table = timetable(GROUP, DATE)
                 channel = client.get_channel(CHANNEL)
